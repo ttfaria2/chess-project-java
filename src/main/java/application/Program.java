c0d488adcd31720b432f56bb9058c7dabf6243a8
ttfaria2 <ttfaria2@gmail.com>
2020-09-17 11:22:51 -0300

    classes board e piece criadas

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import boardgame.Position;
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+
+        sc.close();
+    }
+}
